8562841432807961520ed0f2f77a80cb4eed8d2d
m-h-miller <m.harry.miller@gmail.com>
2015-10-02 11:59:15 -0400

    Write helper bracket method

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,7 +3,6 @@ require 'byebug'
 class Board
 
   attr_reader :bomb_coords
-
   BOMB_COUNT = 10
 
   def initialize
@@ -13,14 +12,26 @@ class Board
     place_bombs
   end
 
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  # def []=(pos, val)
+  #
+  # end
 
   private
-
   def place_bombs
     until @bomb_coords.length == BOMB_COUNT
       x = (0..8).to_a.sample
       y = (0..8).to_a.sample
-      @bomb_coords << [x, y] unless @bomb_coords.include?( [x, y] )
+      unless @bomb_coords.include?( [x, y] )
+        @bomb_coords << [x, y]
+      end
+    end
+
+    @bomb_coords.each do |pos|
+      self[pos].is_bomb = true
     end
   end
 
@@ -28,8 +39,11 @@ end
 
 
 class Tile
-  def initialize(board)
-    
+  attr_accessor :status, :is_bomb
+
+  def initialize
+    @status = :hidden
+    @is_bomb = false
   end
 
   def reveal
